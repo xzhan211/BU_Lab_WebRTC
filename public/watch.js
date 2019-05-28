a68646f8f0f4f5fa8a894051f140d95bb6573890
Adam Nieto <anieto1@binghamton.edu>
2019-05-27 21:37:00 -0400

    cleaned up code

--- public/watch.js
+++ public/watch.js
@@ -1,17 +1,27 @@
-/*global socket, video, config*/
-let peerConnection;
+var video1 = document.getElementById('live_video1');
+video1.width = 640;
+video1.height = 360;
+video1.loop = true;
+video1.muted = true;
+video1.controls = true;
+video1.autoplay = true;
+video1.setAttribute('playsinline', '');
 
+let peerConnection;
 socket.on('offer', function(id, description) {
   peerConnection = new RTCPeerConnection(config);
   peerConnection.setRemoteDescription(description)
-    .then(() => peerConnection.createAnswer())
-    .then(sdp => peerConnection.setLocalDescription(sdp))
-    .then(function () {
-      socket.emit('answer', id, peerConnection.localDescription);
-    });
+  .then(() => peerConnection.createAnswer())
+  .then(sdp => peerConnection.setLocalDescription(sdp))
+  .then(function () {
+    socket.emit('answer', id, peerConnection.localDescription);
+  });
+
   peerConnection.onaddstream = function(event) {
-    video.srcObject = event.stream;
-  };
+      video1.srcObject = event.stream;
+
+
+  }
   peerConnection.onicecandidate = function(event) {
     if (event.candidate) {
       socket.emit('candidate', id, event.candidate);
@@ -20,8 +30,8 @@ socket.on('offer', function(id, description) {
 });
 
 socket.on('candidate', function(id, candidate) {
-    peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
-        .catch(e => console.error(e));
+  peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+  .catch(e => console.error(e));
 });
 
 socket.on('connect', function() {
@@ -29,18 +39,22 @@ socket.on('connect', function() {
 });
 
 socket.on('broadcaster', function() {
-    socket.emit('watcher');
+  socket.emit('watcher');
 });
 
 socket.on('bye', function() {
   peerConnection.close();
 });
 
+let receivedPeerId = null;
+let conn = null;
+let peer = new Peer();
 
-
-
-
-
-/*
- *   send message
- *   */
+socket.on('peerId_test', function(data) {
+    receivedPeerId = data;
+    console.log("This peerId is from broadcast part >>  "+receivedPeerId);
+    conn = peer.connect(receivedPeerId);
+    conn.on('open', () => {
+      conn.send('Done');
+    });
+});
